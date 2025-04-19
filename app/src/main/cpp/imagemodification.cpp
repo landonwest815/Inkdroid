c78f1851715f335d83b29c5e4d01c775bb037098
landoncevans <73617987+landoncevans@users.noreply.github.com>
2025-04-18 19:19:22 -0600

    blur test

--- app/src/main/cpp/imagemodification.cpp
+++ app/src/main/cpp/imagemodification.cpp
@@ -1,7 +1,11 @@
 #include <jni.h>
 #include <android/log.h>
 #include <android/bitmap.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 
+using namespace cv;
 
 #define  LOG_TAG    "imageModification"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
@@ -20,6 +24,24 @@ Java_com_example_drawingappall_jni_ImageModification_blur(JNIEnv *env, jobject t
 
 
     AndroidBitmap_unlockPixels(env, bitmap);*/
+
+
+    AndroidBitmapInfo info;
+    void *pixels;
+    AndroidBitmap_getInfo(env, bitmap, &info);
+    AndroidBitmap_lockPixels(env, bitmap, &pixels);
+
+    // Convert to OpenCV Mat
+    Mat tmp(info.height, info.width, CV_8UC4, pixels);
+
+    // Copy original to output and apply Gaussian blur
+    Mat result;
+    GaussianBlur(tmp, result, Size(5, 5), 0);
+
+    // Copy back
+    result.copyTo(tmp);
+
+    AndroidBitmap_unlockPixels(env, bitmap);
 }
 
 extern "C"
